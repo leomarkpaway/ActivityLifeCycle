04f52f6e6fceebadbc5e484238834f6c553f8125
leomark paway <leomarkpaway@gmail.com>
2024-07-01 12:32:52 +0800

    :sparkles: create toast extension and implement

--- app/src/main/java/com/leomarkpaway/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/activitylifecycle/MainActivity.kt
@@ -1,44 +1,50 @@
 package com.leomarkpaway.activitylifecycle
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showToastLong("onCreate")
+    }
+
+    private fun Context.showToastLong(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("Status","onStart")
+        showToastLong("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("Status","onResume")
+        showToastLong("onResume")
     }
 
 
     override fun onPause() {
         super.onPause()
-        Log.d("Status","onPause")
+        showToastLong("onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("Status","onStop")
+        showToastLong("onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("Status","onDestroy")
+        showToastLong("onDestroy")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("Status","onRestart")
+        showToastLong("onRestart")
     }
 
 }
