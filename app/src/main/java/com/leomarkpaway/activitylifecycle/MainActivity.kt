1f1bee9a48ad7b1ef3e446a34619085e3759991d
leomark paway <leomarkpaway@gmail.com>
2024-07-01 12:27:09 +0800

    :sparkles: add logs activity lifecycle

--- app/src/main/java/com/leomarkpaway/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/activitylifecycle/MainActivity.kt
@@ -2,10 +2,43 @@ package com.leomarkpaway.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("Status","onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("Status","onResume")
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("Status","onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("Status","onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Status","onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("Status","onRestart")
+    }
+
 }
